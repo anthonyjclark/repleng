2372864058afd11a2c0edf57a58279a18ba3ef97
anthonyjclark <anthonyjclark@gmail.com>
2026-02-14 12:34:16 -0800

    Moved basic vite example into comment.

--- src/main.ts
+++ src/main.ts
@@ -32,3 +32,23 @@ const editor = basicEditor(
 // `
 
 // setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+// counter.ts
+// export function setupCounter( element: HTMLButtonElement ) {
+
+// 	let counter = 0;
+// 	const setCounter = ( count: number ) => {
+
+// 		counter = count;
+// 		element.innerHTML = `count is ${counter}`;
+
+// 	};
+
+// 	element.addEventListener( 'click', () => {
+
+// 		setCounter( counter + 1 );
+
+// 	} );
+// 	setCounter( 0 );
+
+// }
